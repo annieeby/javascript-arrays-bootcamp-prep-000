85ca68d0d6769abe9abae4601fe64f92b9f2a26d
Annie Eby <eby.annie@gmail.com>
2018-09-12 21:23:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,7 @@ function addElementToBeginningOfArray(array, element){
   return chocolateBars
 }
 
+<<<<<<< HEAD
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
@@ -48,3 +49,41 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1) 
 }
+=======
+/*
+
+function destructivelyAddElementToBeginningOfArray(array, element) {return
+${array}.unshift(${element})
+}
+
+destructivelyAddElementToBeginningOfArray(chocolateBars, "100grand")
+
+function addElementToEndOfArray() {return
+[...chocolateBars, "100grand"]
+}
+
+function destructivelyAddElementToEndOfArray() {return
+chocolateBars.push("100grand")
+}
+
+function accessElementInArray() {return
+(chocolateBars[0])
+}
+
+function destructivelyRemoveElementFromBeginningOfArray() {return
+chocolateBars.shift() 
+}
+
+function removeElementFromBeginningOfArray() {return
+chocolateBars.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray() {return
+chocolateBars.pop()
+}
+chocolateBars
+
+function removeElementFromEndOfArray() {return
+chocolateBars.slice(0, chocolateBars.length - 1) 
+*/
+>>>>>>> 778e82ac086d6c1b94b11a835e181ad72dc82ac2
